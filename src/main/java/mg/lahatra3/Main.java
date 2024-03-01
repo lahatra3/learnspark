25bb049507f1d2ae939ce54f923f0827e860eb64
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-03-01 22:55:01 +0300

    init project...

--- src/main/java/mg/lahatra3/Main.java
+++ src/main/java/mg/lahatra3/Main.java
@@ -1,7 +1,34 @@
 package mg.lahatra3;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello lahatrad...");
+        log.info("Hello lahatrad...");
+
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(3.11);
+        inputData.add(12.235);
+        inputData.add(13.897);
+        inputData.add(87.25578);
+        inputData.add(11.3658);
+
+        log.info("{}", inputData);
+
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("lahatrad")
+                .setMaster("local[*]");
+
+        try(JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+            JavaRDD<Double> myRdd = sparkContext.parallelize(inputData);
+            sparkContext.close();
+        }
     }
 }
